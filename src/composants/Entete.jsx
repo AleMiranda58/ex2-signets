2c15310a9b0abf8ae0047955c2608d902ea9da62
Alejandra Miranda <ale.miranda890@gmail.com>
2021-02-24 19:39:13 -0500

    css du header

--- src/composants/Entete.jsx
+++ src/composants/Entete.jsx
@@ -1,10 +1,16 @@
+import './Entete.scss'; 
 import avatar from '../images/avatar.png';
+
 
 export default function Entete() {
   return (
     <header className="Entete">
       <div className="logo">Signets</div>
       <div className="avatar">Camille Semaan <img src={avatar} alt="Camille Semaan" /></div>
+     
+
+
+     
     </header>
   );
 }
